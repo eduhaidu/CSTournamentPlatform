89805db9088f03b7e62b3822f158d8c786e3fdc9
eduhaidu <edu.haidu@gmail.com>
2025-11-03 11:19:01 +0200

    initialized frontend + backend structure

---
+++ CSTournamentPlatform_Backend/tournamentserver/src/main/java/cs2/tournamentsite/tournamentserver/TournamentserverApplication.java
@@ -0,0 +1,13 @@
+package cs2.tournamentsite.tournamentserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournamentserverApplication.class, args);
+	}
+
+}
---
+++ CSTournamentPlatform_Backend/tournamentserver/src/test/java/cs2/tournamentsite/tournamentserver/TournamentserverApplicationTests.java
@@ -0,0 +1,13 @@
+package cs2.tournamentsite.tournamentserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournamentserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
